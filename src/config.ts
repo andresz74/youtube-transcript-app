e1e595e789e4f23bdf90e510056eb69143d95031
Andres Zenteno <azenteno74@gmail.com>
2024-10-11 14:26:13 -0400

    feat: update config file

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 const config = {
-    apiBaseUrl: 'http://192.168.1.181:3004', // Update this with the actual base URL
-};
-
-export default config;
+    apiBaseUrl: '/api',  // Proxy API requests through Nginx
+  };
+  
+  export default config;
+  
