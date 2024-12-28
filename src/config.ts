ed6ddb6559abced82582283974bf904dda6125e4
Andres Zenteno <azenteno74@gmail.com>
2024-12-28 00:16:01 -0500

    feat: update prod deplyment API

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 const config = {
   apiBaseUrl: isDevelopment
     ? 'https://192.168.1.181/transcript-api'  // In development, point to your Nginx server or local deployment (http://localhost:3004)
-    : '/transcript-api',  // In production, the same /api as Nginx will serve it
+    : 'https://transcript.andreszenteno.com',  // In production, the same /api as Nginx will serve it
 };
 
 export default config;
